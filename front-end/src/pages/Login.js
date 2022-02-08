9fcefd1dd9dd2c432856e72e2a1bbc4fb898b7ae
Javkhaa00 <liplicker0423@gmail.com>
2022-02-08 18:50:58 +0800

    login dashboard register page added with react router dom

---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <section>
+            <p>Login</p>
+        </section>
+    );
+};
+
+export default Login;
---
+++ front-end/src/pages/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <section>
+            <p>Register</p>
+        </section>
+    );
+};
+
+export default Register;
